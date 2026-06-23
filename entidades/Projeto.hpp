d762acbfdc0645359cd8aee2aaf64df66f7833e1
Caio de Castro <caioaraujo137@gmail.com>
2026-06-23 19:13:46 -0300

    feat: entidade Projeto

---
+++ entidades/Projeto.hpp
@@ -0,0 +1,74 @@
+#ifndef PROJETO_HPP
+#define PROJETO_HPP
+
+#include "../dominios/Codigo.hpp"
+#include "../dominios/Nome.hpp"
+#include "../dominios/Data.hpp"
+
+/**
+ * @class Projeto
+ * @brief Entidade que representa um projeto no sistema.
+ *
+ * A classe Projeto armazena as informações principais de um projeto ágil.
+ * Ela é composta pelas classes de domínio Codigo, Nome e Data (para representar
+ * as datas de início e término).
+ * O código do projeto atua como a chave primária (identificador único).
+ */
+class Projeto {
+private:
+    Codigo codigo;
+    Nome nome;
+    Data inicio;
+    Data termino;
+
+public:
+    /**
+     * @brief Armazena o código identificador do projeto.
+     * @param codigo Instância da classe de domínio Codigo.
+     */
+    void setCodigo(const Codigo& codigo);
+
+    /**
+     * @brief Retorna o código identificador do projeto.
+     * @return Instância da classe de domínio Codigo.
+     */
+    Codigo getCodigo() const;
+
+    /**
+     * @brief Armazena o nome do projeto.
+     * @param nome Instância da classe de domínio Nome.
+     */
+    void setNome(const Nome& nome);
+
+    /**
+     * @brief Retorna o nome do projeto.
+     * @return Instância da classe de domínio Nome.
+     */
+    Nome getNome() const;
+
+    /**
+     * @brief Armazena a data de início do projeto.
+     * @param inicio Instância da classe de domínio Data.
+     */
+    void setInicio(const Data& inicio);
+
+    /**
+     * @brief Retorna a data de início do projeto.
+     * @return Instância da classe de domínio Data.
+     */
+    Data getInicio() const;
+
+    /**
+     * @brief Armazena a data de término do projeto.
+     * @param termino Instância da classe de domínio Data.
+     */
+    void setTermino(const Data& termino);
+
+    /**
+     * @brief Retorna a data de término do projeto.
+     * @return Instância da classe de domínio Data.
+     */
+    Data getTermino() const;
+};
+
+#endif // PROJETO_HPP
