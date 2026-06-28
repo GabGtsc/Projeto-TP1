cd765ff981f05f5addd5aec2b523d582a001d223
Gabriel <gab.gtsc@gmail.com>
2026-06-28 13:52:14 -0300

    feat interfaces gestao

--- interfaces/IServicoGestao.hpp
+++ interfaces/IServicoGestao.hpp
@@ -16,19 +16,19 @@ public:
 
   // servicos de projeto ID 5 - 8
   virtual bool criarProjeto(const Projeto &projeto, const Email &emailPO, const Email &emailSM) = 0;
-  virtual bool lerProjeto(const Codigo &codigo, const Projeto &projeto) = 0;
+  virtual bool lerProjeto(const Codigo &codigo, Projeto &projeto) = 0;
   virtual bool atualizarProjeto(const Projeto &projeto) = 0;
   virtual bool excluirProjeto(const Codigo &codigo) = 0;
 
   // servicos de plano de sprint ID 9 - 12
   virtual bool criarPlanoDeSprint(const PlanoDeSprint &plano, const Codigo &codigoProjeto) = 0;
-  virtual bool lerPlanoDeSprint(const Codigo &codigo, const PlanoDeSprint &plano) = 0;
+  virtual bool lerPlanoDeSprint(const Codigo &codigo, PlanoDeSprint &plano) = 0;
   virtual bool atualizarPlanoDeSprint(const PlanoDeSprint &plano) = 0;
   virtual bool excluirPlanoDeSprint(const Codigo &codigo) = 0;
 
   // servicos de historia de usuario ID 13 - 16
   virtual bool criarHistoriaDeUsuario(const HistoriaDeUsuario &historia, const Codigo &codigoProjeto) = 0;
-  virtual bool lerHistoriaDeUsuario(const Codigo &codigo, const HistoriaDeUsuario &historia) = 0;
+  virtual bool lerHistoriaDeUsuario(const Codigo &codigo, HistoriaDeUsuario &historia) = 0;
   virtual bool atualizarHistoriaDeUsuario(const HistoriaDeUsuario &historia) = 0;
   virtual bool excluirHistoriaDeUsuario(const Codigo &codigo) = 0;
 
