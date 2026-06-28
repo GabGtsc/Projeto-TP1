be7d5e58046e1b96fa87367996e6acfcfd264485
Gabriel <gab.gtsc@gmail.com>
2026-06-28 11:40:59 -0300

    fix: Método ler não pode ter argumento const

--- interfaces/IServicoCadastro.hpp
+++ interfaces/IServicoCadastro.hpp
@@ -6,7 +6,7 @@
 class IServicoCadastro {
 public:
   virtual ~IServicoCadastro() = default;
-  virtual bool ler(const Email &email, const Pessoa &pessoa) = 0;
+  virtual bool ler(const Email &email, Pessoa &pessoa) = 0;
   virtual bool criar(const Pessoa &pessoa) = 0;
   virtual bool atualizar(const Pessoa &pessoa) = 0;
   virtual bool excluir(const Email &email) = 0;
